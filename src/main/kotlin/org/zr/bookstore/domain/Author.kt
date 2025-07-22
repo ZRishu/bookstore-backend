1320fa2797cdb0d720f88374a07d1f7bb8a2154d
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 03:21:02 +0530

    add Author entity

---
+++ src/main/kotlin/org/zr/bookstore/domain/Author.kt
@@ -0,0 +1,29 @@
+package org.zr.bookstore.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "authors")
+data class Author(
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+    val id: Long?,
+
+    @Column(name = "name")
+    val name: String,
+
+    @Column(name = "age")
+    val age: Int,
+
+    @Column(name = "description")
+    val description: String,
+
+    @Column(name = "image")
+    val image: String
+)
