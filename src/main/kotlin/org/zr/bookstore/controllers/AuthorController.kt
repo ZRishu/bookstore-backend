3a0c6e22d4db6709f2dd2d97341327fc342e28e8
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 19:13:04 +0530

    add GET method to readOneAuthor in AuthorController

--- src/main/kotlin/org/zr/bookstore/controllers/AuthorController.kt
+++ src/main/kotlin/org/zr/bookstore/controllers/AuthorController.kt
@@ -3,6 +3,7 @@ package org.zr.bookstore.controllers
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,5 +33,13 @@ class AuthorController(
     fun readManyAuthors(): ResponseEntity<List<AuthorDto>> {
         val authors = authorService.listAuthors().map { it.toAuthorDto() }
         return ResponseEntity(authors, HttpStatus.OK)
+    }
+
+
+    @GetMapping(path = ["/{id}"])
+    fun readOneAuthor(@PathVariable id: Long): ResponseEntity<AuthorDto> {
+        return authorService.getSingleAuthor(id)?.toAuthorDto()
+            ?.let { ResponseEntity(it, HttpStatus.OK) }
+            ?: ResponseEntity.notFound().build()
     }
 }
