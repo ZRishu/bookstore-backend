fbd7352dac90af74bb805973f74136ed081441ee
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 19:10:50 +0530

    add GET method to list all Authors in AuthorController

--- src/main/kotlin/org/zr/bookstore/controllers/AuthorController.kt
+++ src/main/kotlin/org/zr/bookstore/controllers/AuthorController.kt
@@ -2,6 +2,7 @@ package org.zr.bookstore.controllers
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -25,5 +26,11 @@ class AuthorController(
             authorDto.toAuthor()
         ).toAuthorDto()
         return ResponseEntity(createdAuthor, HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun readManyAuthors(): ResponseEntity<List<AuthorDto>> {
+        val authors = authorService.listAuthors().map { it.toAuthorDto() }
+        return ResponseEntity(authors, HttpStatus.OK)
     }
 }
