570d79d9f90aa5b3fdf35506c3a1940166711000
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 18:35:49 +0530

    add BookRepository class

--- src/main/kotlin/org/zr/bookstore/repositories/BookRepository.kt
+++ src/main/kotlin/org/zr/bookstore/repositories/BookRepository.kt
@@ -2,7 +2,7 @@ package org.zr.bookstore.repositories
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.zr.bookstore.domain.Book
+import org.zr.bookstore.domain.entities.Book
 
 @Repository
 interface BookRepository : JpaRepository<Book, String> {
