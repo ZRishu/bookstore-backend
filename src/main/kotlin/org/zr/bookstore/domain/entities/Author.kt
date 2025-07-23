9bd57281a129aae0a7d4ae8d4ad81dbc53eb2873
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 18:30:37 +0530

    moved entities to entities folder

--- src/main/kotlin/org/zr/bookstore/domain/entities/Author.kt
+++ src/main/kotlin/org/zr/bookstore/domain/entities/Author.kt
@@ -1,4 +1,4 @@
-package org.zr.bookstore.domain
+package org.zr.bookstore.domain.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
--- src/main/kotlin/org/zr/bookstore/domain/entities/Book.kt
+++ src/main/kotlin/org/zr/bookstore/domain/entities/Book.kt
@@ -1,4 +1,4 @@
-package org.zr.bookstore.domain
+package org.zr.bookstore.domain.entities
 
 import jakarta.persistence.*
 
