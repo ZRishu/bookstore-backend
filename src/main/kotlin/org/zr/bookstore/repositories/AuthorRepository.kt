ffa0ea3f4a3e79d4ee1af858c448f335c4720a05
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-23 18:33:15 +0530

    add AuthorRepository class

--- src/main/kotlin/org/zr/bookstore/repositories/AuthorRepository.kt
+++ src/main/kotlin/org/zr/bookstore/repositories/AuthorRepository.kt
@@ -2,7 +2,7 @@ package org.zr.bookstore.repositories
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.zr.bookstore.domain.Author
+import org.zr.bookstore.domain.entities.Author
 
 @Repository
 interface AuthorRepository : JpaRepository<Author, Long?> {
