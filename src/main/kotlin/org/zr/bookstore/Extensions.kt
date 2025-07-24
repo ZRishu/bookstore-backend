bba2cd114b31097855ac8000275088388ccd0502
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-24 13:23:26 +0530

    map BookUpdateRequestDto to BookUpdateRequest

--- src/main/kotlin/org/zr/bookstore/Extensions.kt
+++ src/main/kotlin/org/zr/bookstore/Extensions.kt
@@ -3,10 +3,12 @@ package org.zr.bookstore
 import org.zr.bookstore.domain.AuthorSummary
 import org.zr.bookstore.domain.AuthorUpdateRequest
 import org.zr.bookstore.domain.BookSummary
+import org.zr.bookstore.domain.BookUpdateRequest
 import org.zr.bookstore.domain.dtos.AuthorDto
 import org.zr.bookstore.domain.dtos.AuthorSummaryDto
 import org.zr.bookstore.domain.dtos.AuthorUpdateRequestDto
 import org.zr.bookstore.domain.dtos.BookSummaryDto
+import org.zr.bookstore.domain.dtos.BookUpdateRequestDto
 import org.zr.bookstore.domain.entities.Author
 import org.zr.bookstore.domain.entities.Book
 import org.zr.bookstore.exceptions.InvalidAuthorException
@@ -72,5 +74,11 @@ fun Author.toAuthorSummaryDto(): AuthorSummaryDto {
 fun AuthorSummaryDto.toAuthorSummary() = AuthorSummary(
     id = this.id,
     name = this.name,
+    image = this.image
+)
+
+fun BookUpdateRequestDto.toBookUpdateRequest() = BookUpdateRequest(
+    title = this.title,
+    description = this.description,
     image = this.image
 )
