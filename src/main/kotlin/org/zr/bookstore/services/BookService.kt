fdf5143c3d81c9dc30a0cf1a3612657083e7cc46
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-24 10:51:19 +0530

    add add create and update book method in service layer

---
+++ src/main/kotlin/org/zr/bookstore/services/BookService.kt
@@ -0,0 +1,8 @@
+package org.zr.bookstore.services
+
+import org.zr.bookstore.domain.BookSummary
+import org.zr.bookstore.domain.entities.Book
+
+interface BookService {
+    fun createUpdate(isbn: String, bookSummary: BookSummary): Pair<Book, Boolean>
+}
---
+++ src/main/kotlin/org/zr/bookstore/services/Impl/BookServiceImpl.kt
@@ -0,0 +1,33 @@
+package org.zr.bookstore.services.Impl
+
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.zr.bookstore.domain.BookSummary
+import org.zr.bookstore.domain.entities.Book
+import org.zr.bookstore.repositories.AuthorRepository
+import org.zr.bookstore.repositories.BookRepository
+import org.zr.bookstore.services.BookService
+import org.zr.bookstore.toBook
+
+@Service
+class BookServiceImpl(
+    private val bookRepository: BookRepository,
+    private val authorRepository: AuthorRepository
+) : BookService {
+
+    @Transactional
+    override fun createUpdate(
+        isbn: String,
+        bookSummary: BookSummary
+    ): Pair<Book, Boolean> {
+        val normalisedBook = bookSummary.copy(isbn = isbn)
+        val isExists = bookRepository.existsById(isbn)
+
+        val author = authorRepository.findByIdOrNull(normalisedBook.author.id)
+        checkNotNull(author)
+
+        val savedBook = bookRepository.save(normalisedBook.toBook(author))
+        return Pair(savedBook, !isExists)
+    }
+}
