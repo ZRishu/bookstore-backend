eb9039a8d958ad0ace641d206c9952911937d7d4
ZRishu <161687207+ZRishu@users.noreply.github.com>
2025-07-24 10:51:52 +0530

    add author and book summary entity

---
+++ src/main/kotlin/org/zr/bookstore/domain/BookSummary.kt
@@ -0,0 +1,9 @@
+package org.zr.bookstore.domain
+
+data class BookSummary(
+    val isbn: String,
+    val title: String,
+    val description: String,
+    val image: String,
+    val author: AuthorSummary
+)
